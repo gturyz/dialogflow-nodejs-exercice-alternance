cffb0f07064e0d2ed4569d9331bcfd41918a3304
Léo Bonhême--Morsent <gturyz@gmail.com>
2020-07-03 16:16:11 +0200

    Fonction chatbot

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChatBot/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatBot from './components/ChatBot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatBot
   }
 }
 </script>
---
+++ src/components/ChatBot.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="chatbot">
+    <div>
+      <ul class="list">
+        <li class="message" v-for="(message, index) in messages" :key="index" :class="message.auteur">
+           <p>
+             <span>{{ message.text }}</span>
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="input">
+      <input type="text" v-model="message" @keyup.enter="envoyerMessage">
+      <button @click="envoyerMessage">Envoyer</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ChatBot',
+  data: () => ({
+    message: '',
+    messages: [{
+      text: 'Bonjour, qu\'elle est votre question ?',
+      auteur: 'server'
+    }]
+  }),
+  methods: {
+    envoyerMessage() {
+      this.messages.push({
+        text: this.message,
+        auteur: 'client'
+      });
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
